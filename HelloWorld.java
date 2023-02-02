6d7672999fbfbcbfc5e1837f601446ff0a3d6c9a
Nelson Lu <nelsonlu3@gmail.com>
2023-02-02 01:53:38 -0600

    Finished Program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,21 @@
+import java.util.Scanner; // For inputs
+
 public class HelloWorld {
     public static void main(String [] args) {
-        
+        Scanner in = new Scanner(System.in); // New Scanner object
+        int numNames = in.nextInt(); // Reading user input of number of names
+
+        in.nextLine(); // Clearing garbage
+
+        String[] names = new String[numNames]; // Array to store names being inputed
+        for (int x = 0; x < numNames; x++) {
+            String nameIN = in.nextLine(); // Reading user input of names
+            names[x] = nameIN; // Storing names in the array
+        }
+
+        // Outputting Hello, names[x]!
+        for (int x = 0; x < names.length; x++) {
+            System.out.println("Hello, " + names[x] + "!");
+        }
     }
 }
